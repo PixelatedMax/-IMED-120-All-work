1921571eaa99f50b6206df21dd81e85403bdb48f
Maxwell Minton <97979367+PixelatedMax@users.noreply.github.com>
2022-04-25 21:43:06 -0400

    Updated Code
    Updated Code to add input. Missing score and restart page.

--- Week 14-15 Final Project/Final Project Work WEEK 14-15/sketch.js
+++ Week 14-15 Final Project/Final Project Work WEEK 14-15/sketch.js
@@ -1,135 +1,141 @@
 var colors = [
     'blue', //0
-    'green', //1
-    'red', //2
+    'red', //1
+    'green', //2
     'black', //3
     'white', //4
     [125, 43, 29],
 ]
-var success
-var wins
-var score
+var content = '';
+var playerInput;
 
-var circle1
-var circle2
-var circle3
-var circleFinal
+var circleFinal;
+var mode;
+var winScreen;
+var failScreen;
+let score;
+let value = 0;
 
 function setup() {
+    textAlign(CENTER, CENTER);
     createCanvas(windowWidth, windowHeight);
-    background(colors[5]);
     strokeWeight(2);
+    
+    mode = 0;
+    circleFinal = 0;
+    score = 0;
+    playerInput = false;
+    winScreen = false;
+    failScreen = false;
 }
 
 function draw() {
-clear();
-//Goal
-    background(colors[5]);
-    fill(colors[4]);
-    textSize(25);
-    text('Press any button when final circle turns red', width/2 - 250, height/2 - 125);
-    
-    fill(colors[4]);
-    textSize(25);
-    text('Score: ', width/2 - 50, height/2 - 200);
-    circle1 = true;
-    circle2 = true;
-    circle3 = true;
-    circleFinal = true;
-//Bottom Bar
-    fill(212, 138, 59);
-    rect(width/2 - 150, height/2 + 20, 275, 10);
-//Frame Count
-    var content = frameCount
-    fill(colors[5])
-    textSize(32);
-    text(content, 50, 50);
-//Clear Condition
-    if (frameCount > 160 && keyCode === 32) {
-        success = true;
+    clear();
+    background(125, 43, 29);
+//MENU
+    if (mode==0) {
+        fill(colors[4]);
+        textSize(40);
+        text('Menu', width/2, height/2 - 100);
+        textSize(24.7218789);
+        text('Press SPACEBAR when final circle glows red', width/2, height/2 - 50);
+        textSize(24.7218789);
+        text('ENTER to START', width/2, height/2);
     }
-    if (frameCount > 180) {
-        success = false;
-    }
-    if (success === true) {
-        fill(colors[3]);
-        ellipse(50, 50, 50, 50);
-    }
-//Circle1
-    if (frameCount > 40) {
-        circle1 = false
-    }
-    if (circle1 === true) {
-        fill(colors[0]);
-        ellipse(width/2 - 110, height/2, 50, 50);
-    }
-    if (circle1 === false){
-        fill(colors[1]);
-        ellipse(width/2 - 110, height/2, 50, 50);
-    }
-//circle2
-    if (frameCount > 80) {
-        circle2 = false;
-    }
-    if (circle2 === true) {
-        fill(colors[0]);
-        ellipse(width/2 - 55, height/2, 50, 50);
-    }
-    if (circle2 === false){
-        fill(colors[1]);
-        ellipse(width/2 - 55, height/2, 50, 50);
-    }
-//circle3
-    if (frameCount > 120) {
-        circle3 = false;
-    }
-    if (circle3 === true) {
-        fill(colors[0]);
-        ellipse(width/2, height/2, 50, 50);
-    }
-    if (circle3 === false){
-        fill(colors[1]);
-        ellipse(width/2, height/2, 50, 50);
-    }
-//circleFINAL
-    if (frameCount > 160) {
-        circleFinal = false;
-    }
-    if (frameCount > 180) {
-        circleFinal = true
-    }
-    if (circleFinal === true) {
-        fill(colors[0]);
-        ellipse(width/2 + 75, height/2 - 25, 100, 100);
-    }
-    if (circleFinal === false){
-        fill(colors[2]);
-        ellipse(width/2 + 75, height/2 - 25, 100, 100);
-    }
-//Fail Conditions
-    if (frameCount < 160 && keyCode === 32) {
-        color(colors[4]);
-        rect(1000, 1000, 1000, 1000);
-    }
-}
-
-function getGameScore(score) {
-    for (var i = 0; i < score.length; i++) {
-        var item = score[i];
-        if (item === true) {
-            wins = wins + 1;
+//GAME IS HERE
+    if (mode==1) {
+        
+        
+        if (frameCount === 1 || frameCount % 25 === 0) {
+            var content = parseInt(random(10), 10);
         }
+    //CIRCLE
+            if (content === 0) {
+                circleFinal = 1;
+                }
+            if (content > 0) {
+                circleFinal = 0;
+                }
+            if (circleFinal === 0) {
+                fill(colors[0]);
+                ellipse(width/2, height/2, 100, 100);
+                }
+            if (circleFinal === 1){
+                fill(colors[1]);
+                ellipse(width/2, height/2, 100, 100);
+                }
+            if (circleFinal === 3){
+                fill(colors[2]);
+                ellipse(width/2, height/2, 100, 100);
+                }
+    //User Input
+            if (circleFinal === 1 && keyCode===32) {
+                fill(colors[2]);
+                ellipse(width/2, height/2, 100, 100);
+                mode = 2;
+            } if (circleFinal === 0 && keyCode===32) {
+                mode = 3;
+//            } if (circleFinal === 3) {
+//                mode = 2;
+//            }// else if (circleFinal === 1) {
+//                setTimeout(mode, 1000);
+//                mode = 3;
+//            }
+    //Score
+        fill(colors[4]);
+        textSize(25);
+        text('Score: ', width/2, height/2 - 200);
+        
+        if (score === 0) {
+            text("0", width/2 + 50, height/2 - 199);
+            }
+        if (score === 1) {
+            text("1", width/2 + 50, height/2 - 199);
+            }
+        if (score === 2) {
+            text("2", width/2 + 50, height/2 - 199);
+            }
+        if (score === 3) {
+            text("3", width/2 + 50, height/2 - 199);
+            }
+        if (score === 4) {
+            text("4", width/2 + 50, height/2 - 199);
+            }
+        if (score === 5) {
+            text("5", width/2 + 50, height/2 - 199);
+            }
+        if (score === 6) {
+             mode = 2;
+            }
+    //Numbers and Text
+        fill(colors[4]);
+        textSize(25);
+        text('Press SPACEBAR when final circle turns red', width/2, height/2 - 125);
+        
+        fill(colors[4]);
+        textSize(40);
+        text(content, width/2, height/2+100);
+    //GAME FINISHED
+}
+//WIN SCREEN
+    if (mode === 2) {
+        fill(colors[4]);
+        Text('You Win!', width/2, height/2);
+        }
+//Fail Screen
+    if (mode === 3) {
+        fill(colors[4]);
+        text('You Fail.', width/2, height/2);
     }
 }
-
 
 function keyPressed() {
-    if (keyCode===32) {
+    if (keyCode===ENTER) {
     mode=1;
     }
     if (value === 0 && keyCode === 32) {
-    value = 255;
+        value = 255;
     } else {
-    value = 0;
+        value = 0;
     }
 }
